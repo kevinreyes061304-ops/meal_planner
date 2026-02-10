4b8c1b26bb61e75e90502676c312e88a5f851711
Kevin <kevinreyes061304@gmail.com>
2026-02-10 17:54:29 +0800

    build

--- build_file.sh
+++ build_file.sh
@@ -9,3 +9,7 @@ python manage.py collectstatic --noinput
 # Make migrations
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+
+#!/bin/bash
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
